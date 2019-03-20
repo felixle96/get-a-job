9bf3da213487593285953a34ff10b10adc9f45d2
Felix <felixle96@gmail.com>
2019-03-20 10:13:26 -0700

    Added gitignore and working on jukebox.js

---
+++ CodingTheCrackingInterview/Object-OrientedDesign/jukebox.js
@@ -0,0 +1,42 @@
+const play = require('audio-play');
+const load = require('audio-loader');
+ 
+let playBack = load('C:/Users/FelixLe/Music/Songs/"James TW - Ex (Acoustic)"').then(function(audioBuffer) {
+    return play(audioBuffer, {
+        start: 0,
+        end: audioBuffer.duration,
+        rate: 1,
+        volume: 8,
+        autoplay: true
+    });
+});
+class Artist {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Song {
+    constructor(name, artist, file) {
+        this.name = name;
+        this.artist = artist;
+        this.file;
+    }
+}
+
+class CD {
+    constructor(title, songs) {
+        this.title = tile;
+        this.songs = songs;
+    }
+
+    getNumTracks() {
+        return this.songs.length;
+    }
+
+    getTracks() {
+        return this.songs;
+    }
+
+
+}
